250981bb39e12eccc99da74d12d69c67f5e09afb
Eliza <emdanyi@gmail.com>
2015-08-06 02:35:22 -0400

    add start_id and count

--- public/index.php
+++ public/index.php
@@ -16,14 +16,30 @@ require_once "../app/config.local.php";
 
 $db = new PDO($config['db']['dsn'], $config['db']['user'], $config['db']['pass']);
 
-function test($db) {
-    $sql = 'SELECT * FROM Quotes';
+function test($db, $start_id=1, $count=10) {
+
+    $sql = 'SELECT `quotation`, `author` FROM Quotes
+        JOIN `authors` ON `authors`.`a_id`= quotes.`author_id`
+        LIMIT ' . $start_id . ', ' . $count;
+
     foreach ($db->query($sql) as $row) {
-        echo "<li>" . $row['q_id'] ." - ". $row['quotation'] ." - ". $row['author_id'] . "</li>\n";
+        echo "<li>" . $row['quotation'] ." - ". $row['author'] . "</li>\n";
     }
 }
 
-test($db);
+$start_id = 1;
+$count = 10;
+
+if (isset($_GET['start_id'])) {
+    $start_id = $_GET['start_id'];
+}
+
+if (isset($_GET['count'])) {
+    $count = $_GET['count'];
+}
+
+test($db, $start_id, $count);
+
 
 ?>
     </ol>
