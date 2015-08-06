a053cc30c1b341dbef43088faf9eb9114f194987
Eliza <emdanyi@gmail.com>
2015-08-06 03:04:13 -0400

    add SQL-injection protection

--- public/index.php
+++ public/index.php
@@ -20,9 +20,16 @@ function test($db, $start_id=1, $count=10) {
 
     $sql = 'SELECT `quotation`, `author` FROM Quotes
         JOIN `authors` ON `authors`.`a_id`= quotes.`author_id`
-        LIMIT ' . $start_id . ', ' . $count;
+        LIMIT :start_id, :count';
 
-    foreach ($db->query($sql) as $row) {
+    $stmt = $db->prepare($sql);
+
+    $stmt->bindParam(':start_id', $start_id, PDO::PARAM_INT);
+    $stmt->bindParam(':count', $count, PDO::PARAM_INT);
+
+
+    $stmt->execute();
+    foreach ($stmt->fetchAll() as $row) {
         echo "<li>" . $row['quotation'] ." - ". $row['author'] . "</li>\n";
     }
 }
@@ -30,16 +37,16 @@ function test($db, $start_id=1, $count=10) {
 $start_id = 1;
 $count = 10;
 
-if (isset($_GET['start_id'])) {
+if (isset($_GET['start_id']) && preg_match('/^\d{,5}$/', $_GET['start_id'])) {
     $start_id = $_GET['start_id'];
 }
-
-if (isset($_GET['count'])) {
+if (isset($_GET['count'])&& preg_match('/^\d{,3}$/', $_GET['count'])) {
     $count = $_GET['count'];
 }
 
-test($db, $start_id, $count);
 
+
+test($db, $start_id, $count);
 
 ?>
     </ol>
