2a3a1f3c8d66fdb87d66061799a8f839f9923a00
Eliza <emdanyi@gmail.com>
2015-08-06 01:11:35 -0400

    ?

--- public/index.php
+++ public/index.php
@@ -1,3 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+
+
+<head>
+    <title>YAY</title>
+    <meta charset="utf-8">
+</head>
+
+
+<body>
+    <ol>
 <?php
 
-require_once "../app/app.php";
+require_once "../app/config.local.php";
+
+$db = new PDO($config['db']['dsn'], $config['db']['user'], $config['db']['pass']);
+
+function test($db) {
+    $sql = 'SELECT * FROM Quotes';
+    foreach ($db->query($sql) as $row) {
+        echo "<li>" . $row['q_id'] ." - ". $row['quotation'] ." - ". $row['author_id'] . "</li>\n";
+    }
+}
+
+test($db);
+
+?>
+    </ol>
+
+</body>
+
+</html>
